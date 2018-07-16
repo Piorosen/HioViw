6b7f0ad28fbcbb04d936cf3e1f945f71e9b068a6
Aoi Kazto <ckwngud1@naver.com>
2018-07-16 20:34:32 +0900

    [Search Engine]

--- HioViw/Search Engine/SearchEngine.cs
+++ HioViw/Search Engine/SearchEngine.cs
@@ -37,7 +37,34 @@ namespace HioViw
                     JArray list = JArray.Parse(sr.ReadToEnd());
                     for (int w = 0; w < list.Count; w++)
                     {
-                        MessageBox.Show(list[w]["t"].ToString());
+                        JObject j = list[w].ToObject<JObject>();
+                        
+                        foreach (var content in j)
+                        {
+                            if (search.Title.Length != 0 && content.Key == "n")
+                            {
+
+                            }
+                            else if (search.Language.Count != 0 && content.Key == "l")
+                            {
+
+                            }
+                            else if (search.Type.Count != 0 && content.Key == "type")
+                            {
+
+                            }
+                            else if (search.Character.Count != 0 && content.Key == "c")
+                            {
+
+                            }
+                            else if (search.Series.Count != 0 && content.Key == "p")
+                            {
+
+                            }else if (search.Tags.Count != 0 && content.Key == "t")
+                            {
+
+                            }
+                        }
                     }
 
 
